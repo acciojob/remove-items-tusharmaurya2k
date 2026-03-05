3eb595a595e713a2b010e725acb8c70b2f172316
tusharmaurya2k <89917676+tusharmaurya2k@users.noreply.github.com>
2026-03-06 02:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const btn = document.querySelector('input[type="button"]');
+
+btn.onclick = function () {
+  const select = document.getElementById("colorSelect");
+  select.remove(select.selectedIndex);
+};
